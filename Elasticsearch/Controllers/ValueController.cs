bdc04d22c87c4d3867d4d8708e49bc979bb87608
root <pearsink>
2020-06-24 23:23:18 +0800

    ‘
    提交代码

---
+++ Elasticsearch/Controllers/ValueController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elasticsearch.Model;
+using Elasticsearch.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Nest;
+using NF.Common.Utility;
+
+namespace Elasticsearch.Controllers
+{
+
+    public class ValueController : ControllerBase
+    {
+        private readonly ElasticClient _client;
+
+        public ValueController(IEsClientProvider clientProvider)
+        {
+            _client = clientProvider.GetClient();
+        }
+
+        [HttpPost]
+        [Route("value/index")]
+        public IIndexResponse Index(Post post)
+        {
+            //Post post = new Post();
+            return _client.IndexDocument(post);
+        }
+
+        [HttpPost]
+        [Route("value/search")]
+        public IReadOnlyCollection<Post> Seaarch(string type)
+        {
+            Singleton.getInstance().Init("ValueController").Error("demo .net core search error !!!!!");
+            return _client.Search<Post>(s => s
+                .From(0)
+                .Size(10)
+                .Query(q => q.Match(m => m.Field(f => f.Type).Query(type)))).Documents;
+        }
+    }
+}
---
+++ Elasticsearch/Startup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Elasticsearch.Service;
+using log4net;
+using log4net.Config;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NF.Common.Utility;
+
+namespace Elasticsearch
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<IEsClientProvider, EsClientProvider>();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            Log4netHelper.Repository = LogManager.CreateRepository("NETCoreRepository");
+            XmlConfigurator.Configure(Log4netHelper.Repository, new FileInfo(Environment.CurrentDirectory + "/log4net.config"));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvc();
+        }
+    }
+}
